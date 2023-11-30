c4a6b3d25cdb4d4da205c09679bf85ed53896467
victorlucaszx <44774448+victorlucaszx@users.noreply.github.com>
2023-11-30 18:32:04 +0000

    adiciona scripts e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação de um tabnews com o curso.dev para aprender programação
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-function Home(){
-    return <h1>Modificando a frase </h1>
+function Home() {
+  return <h1>Modificando a frase </h1>;
 }
 export default Home;
